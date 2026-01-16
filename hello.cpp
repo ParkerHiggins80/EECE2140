169f92f7d457606507f4cc53aa4476b3fbdca6c3
ParkerHiggins80 <higgins.par@northeastern.edu>
2026-01-16 12:08:20 -0500

    Add assignment files

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello from EECE 2140 on WSL2!\n";
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num;
+    num = 10;
+    int tempNum = 2*num;
+    cout <<"Num = "<<num<<", tempNum = "<<tempNum<<endl;
+    return 0;
+}
